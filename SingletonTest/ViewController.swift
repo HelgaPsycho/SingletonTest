f7681d8819301cbeca43e1a35b542c5bfe43f28a
Ольга Егорова <hardcorecase@gmail.com>
2023-01-19 16:08:13 +0300

    Первый и последний коммит

--- SingletonTest/ViewController.swift
+++ SingletonTest/ViewController.swift
@@ -12,8 +12,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print(Settings.shared.volumeLevel)
+        
+        Settings.shared.volumeLevel = 2.0
+        
+        print(Settings.shared.volumeLevel)
+        
+        //Apple использует Singleton в UIApplication, UserDefaults, URLSession
+        // ex:
+        UIApplication.shared.applicationIconBadgeNumber = 1
+        //Добавляет бейдж 1 к иконке приложени
+        
+       // UserDefaults.standard
+      //  URLSession.shared // так можно воспользоваться готовой сессией от Apple
     }
-
 
 }
 
